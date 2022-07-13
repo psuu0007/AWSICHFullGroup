a42caefddef50c375422ed39298d9853c11b191e
EZEN-216T <EZEN-216T@5-t>
2022-07-13 16:33:04 +0900

    수강생들에게 보내는 과제
    반복문과 조건문을 활용하여
    짝수들의 합을 구하시오

---
+++ GitStudyTest1/src/ForApplyExam.java
@@ -0,0 +1,24 @@
+public class ForApplyExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum = 0;
+		
+		for (int i = 0; i <= 10; i++) {
+//			System.out.println("i가 2인 경우에만 수행되는 조건문");
+			if(i == 2) {
+				sum = sum + i;
+				System.out.println("i의 현재 위치: " + i + " - 합계: " + sum);
+				
+				System.out.println();
+			}
+			
+//			System.out.print("짝수의 합: " + sum);
+//			System.out.println();
+		}
+		
+		System.out.println("0~10까지의 짝수 합계는: " + sum);
+	}
+
+}
