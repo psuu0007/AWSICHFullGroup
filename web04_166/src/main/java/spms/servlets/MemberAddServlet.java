0e15f14e9e67ce53dd3564bbe183bc97f3c400b7
EZEN-216T <EZEN-216T@5-t>
2022-09-14 13:54:24 +0900

    서블릿으로 회원가입 화면 만들기 수행 중

---
+++ web04_166/src/main/java/spms/servlets/MemberAddServlet.java
@@ -0,0 +1,27 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 신규 회원 추가
+public class MemberAddServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+		throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, res);	
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+		throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, res);
+	}
+	
+}
--- web04_166/src/main/java/spms/servlets/MemberListServlet.java
+++ web04_166/src/main/java/spms/servlets/MemberListServlet.java
@@ -29,16 +29,23 @@ public class MemberListServlet extends GenericServlet{
 		String password = "ezen";
 		
 		try {
+			// 1단계 db 드라이버 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			// 2단계 데이터베이스 연결
 			conn = DriverManager.getConnection(url, user, password);
+			
+			// 3단계 sql 실행 객체 준비
 			stmt = conn.createStatement();
 			
 			String sql = "SELECT MNO, MNAME, EMAIL, CRE_DATE"
 				+ " FROM MEMBERS"
 				+ " ORDER BY MNO ASC";
 			
+			// 4단계 sql문 수행
 			rs = stmt.executeQuery(sql);
 			
+			// 5단계 select 결과물 활용하기
 			res.setContentType("text/html");
 			res.setCharacterEncoding("UTF-8");
 			
@@ -64,6 +71,7 @@ public class MemberListServlet extends GenericServlet{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
+			// 6 단계 jdbc 객체 메모리 회수
 			if(rs != null) {
 				try {
 					rs.close();
@@ -93,9 +101,7 @@ public class MemberListServlet extends GenericServlet{
 		}
 		
 		
-		
 	}
-
 	
 	
 }
