4cf4e4ee2424e954ea89e7f0b78f19a40633da66
EZEN-216T <EZEN-216T@5-t>
2022-09-14 14:47:13 +0900

    회원등록 페이지 완성 get 구현
    db연동은 아직 안함

--- web04_166/src/main/java/spms/servlets/MemberAddServlet.java
+++ web04_166/src/main/java/spms/servlets/MemberAddServlet.java
@@ -1,6 +1,7 @@
 package spms.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,14 +15,34 @@ public class MemberAddServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 		throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, res);	
+		
+		res.setContentType("text/html");
+		res.setCharacterEncoding("UTF-8");
+		PrintWriter out = res.getWriter();
+		
+		String htmlStr = "";
+		
+		htmlStr += "<html><head><title>회원 등록</title></head>";        
+		htmlStr += "<body>";
+		htmlStr += "<h1>회원등록</h1>";
+		htmlStr += "<form action='add' method='post'>";
+		htmlStr += "이름: <input type='text' name='name'><br>";
+		htmlStr += "이메일: <input type='text' name='email'><br>";
+		htmlStr += "암호: <input type='password' name='password'><br>";
+		htmlStr += "<input type='submit' value='추가'>";
+		htmlStr += "<input type='reset' value='취소'>";
+		htmlStr += "</form>";
+		htmlStr += "</body></html>";
+		
+		out.print(htmlStr);
+		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 		throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doPost(req, res);
+		
 	}
 	
 }
